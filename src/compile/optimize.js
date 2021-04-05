4110758d577734f95a9344f4e73ffd1c9a9ff21f
LebsterFace <samsalinsky@gmail.com>
2021-04-05 08:52:05 +0100

    Update optimization

--- src/compile/optimize.js
+++ src/compile/optimize.js
@@ -1,12 +1,23 @@
+function optimize(brainF, n = 0) {
+    brainF = brainF.replace(/\[\]/g, "");
+
+    const inversePairs = (achar, bchar) => brainF.replace(new RegExp(`(\\${achar}+)(\\${bchar}+)`, "g"), (_, a, b) => {
+        if (a.length > b.length) {
+            return achar.repeat(a.length - b.length);
+        } else {
+            return bchar.repeat(b.length - a.length);
+        }
+    });
+
+    brainF = inversePairs(">", "<");
+    brainF = inversePairs("<", ">");
+    brainF = inversePairs("+", "-");
+    brainF = inversePairs("-", "+");
+    brainF = inversePairs("[", "]");
+
+    return n === 0 ? brainF : optimize(brainF, n - 1);
+}
+
 module.exports = function(input) {
-    let brainF = input.replace(/[^<>+,\.\[\]+\-]/g, "");
-
-    brainF = brainF
-            .replace(/<>/g, "")
-            .replace(/\+-/g, "")
-            .replace(/-\+/g, "")
-            .replace(/\[\]/g, "")
-            .replace(/></g, "");
-
-    return brainF;
+    return optimize(input.replace(/[^<>+,\.\[\]+\-]/g, ""), 10);
 };
