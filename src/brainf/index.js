976c020dd70e29c44fcdd7795d096d70e046b0f4
LebsterFace <samsalinsky@gmail.com>
2021-04-07 23:39:28 +0100

    Number generator

--- src/brainf/index.js
+++ src/brainf/index.js
@@ -1,3 +1,5 @@
+const getFromZero = require("./getFromZero.json");
+
 const simpleNumber = (a, b) => {
     if (a > b) {
         return "-".repeat(a - b);
@@ -6,10 +8,18 @@ const simpleNumber = (a, b) => {
     }
 };
 
+const number = (a, b) => {
+    let result = "";
+    if (a !== 0) result += "[-]\n";
+    const digits = b.toString().split("").map(n => parseInt(n));
+    result += digits.map(d => "+".repeat(d)).join("[>++++++++++<-]>");
+    return result;
+};
+
 const changeCells = (start, end) => {
     return start.map((value, index) =>
         simpleNumber(value, end[index])
     ).join(">");
 };
 
-module.exports = {simpleNumber, changeCells};
+module.exports = {simpleNumber, changeCells, number};
